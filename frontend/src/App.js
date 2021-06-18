9d734d4c519346582504fa85739c6ba1db634058
Sifat Ibna Amin <sifatibna.amin9@gmail.com>
2021-06-19 03:14:53 +0600

    added Header and Footer component to our app and also added react-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
 
+import { Container } from "react-bootstrap";
+
+import Header from "./component/Header/Header.component";
+import Footer from "./component/Footer/Footer.component";
+
 const App = () => {
   return (
     <>
-      <h1>Welcome to ALL-SHOP</h1>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome to ALL-SHOP</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 };
