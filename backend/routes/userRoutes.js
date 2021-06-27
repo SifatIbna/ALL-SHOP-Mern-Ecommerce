8f3c40fd18096f989118bc2051e89686da762b87
Sifat Ibna Amin <sifatibna.amin9@gmail.com>
2021-06-27 22:22:12 +0600

    user authentication and autorization using jwt token and created api gateway to login and retrieve data from db and register

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+} from "../controllers/userController.js";
+import protect from "../middlewares/authMiddleware.js";
+
+const router = express.Router();
+
+router.post("/", registerUser);
+router.post("/login", authUser);
+router.get("/profile", protect, getUserProfile);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,18 @@ import colors from "colors";
 
 import { notFound, errorHandler } from "./middlewares/errorMiddlewares.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+app.use(express.json());
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 
